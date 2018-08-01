9a9d6a9c2eda2bf96910e890c683bf3cfff2a947
m-aggarwal <40381237+m-aggarwal@users.noreply.github.com>
2018-08-01 14:19:35 +0530

    Update PermissionsTest.java

--- src/package2/Test/PermissionsTest.java
+++ src/package2/Test/PermissionsTest.java
@@ -4,7 +4,9 @@ package package2.Test;
 import static org.junit.Assert.*;
 
 import java.io.IOException;
-
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.Properties;
 
 import org.json.JSONException;
 
@@ -15,9 +17,42 @@ import package2.Permissions;
 
 public class PermissionsTest {
 
-    String validaddress = Config.getProperty("validaddress");
+    String validaddress ;
 	
-	public PermissionsTest() throws IOException{}
+	
+	Properties prop;
+	
+	 public boolean getPropert() throws IOException {
+
+	         prop = new Properties();
+
+	        String path = "config.properties";
+	        File file = new File(path);
+	        if (file.exists()) {
+	            FileInputStream fs = new FileInputStream(path);
+	            prop.load(fs);
+	            fs.close();
+	            return true;
+	        } else {
+	            return false;
+	        }
+	    }
+	
+	
+	public PermissionsTest() throws IOException{
+	
+	if (getPropert() == true) {
+           
+            validaddress = Config.getProperty("validaddress");
+            
+        } else {
+           
+            validaddress = System.getenv("validaddress");
+           
+        }
+	
+	
+	}
 	
 	@Test
 	public void grantPermission() throws Exception{
